3e08592896c57e4210a02d8b44e8a76e23befa1b
Parhizkar, Seyedmehdi <Seyedmehdi.Parhizkar@finastra.com>
2022-04-14 13:56:52 +0200

    End test unite app-home

--- src/components/app-home/test/app-home.spec.tsx
+++ src/components/app-home/test/app-home.spec.tsx
@@ -1,5 +1,5 @@
 import { newSpecPage, SpecPage } from '@stencil/core/testing';
-import { h } from '@stencil/core';
+import { h, State } from '@stencil/core';
 import { AppHome } from '../app-home';
 import { ITodo } from '../../types';
 
@@ -33,15 +33,16 @@ describe('app-home', () => {
       order: 3,
       title: 'lunch',
       url: 'https://my-list-to-do.netlify.app/',
-    }
-  ]
-  
+    },
+  ];
+
   let page: SpecPage;
   let fetchMock;
   beforeEach(async () => {
-      fetchMock = jest.spyOn(global, 'fetch').mockReturnValue( Promise.resolve({
-      json: () => Promise.resolve(todos),
-    } as Response)
+    fetchMock = jest.spyOn(global, 'fetch').mockReturnValue(
+      Promise.resolve({
+        json: () => Promise.resolve(todos),
+      } as Response),
     );
     page = await newSpecPage({
       components: [AppHome],
@@ -58,35 +59,27 @@ describe('app-home', () => {
     const todoCheckedEvent = new CustomEvent<ITodo>('update-todo', {
       detail: {
         ...TODO_01,
-        completed: true
-      }
+        completed: true,
+      },
     });
-   window.fetch = jest.fn();
-    page.root.dispatchEvent(todoCheckedEvent)
+    window.fetch = jest.fn();
+    page.root.dispatchEvent(todoCheckedEvent);
     expect(fetchMock).toBeTruthy();
   });
 
-// function changeBooleanTotoBeenEdited(x: Readonly<ITodo>) {
-//       x = null;
-//     }
-//   it('should return null the ITodo related to modal', async () => {
-//     const toReturnNull = changeBooleanTotoBeenEdited(TODO_01)
-//     expect(toReturnNull).toReturn(null);
-//   });
-
-  // it('should listen [cancel-modal] event in edit-todo component and cancel the modal of edit-todo', async () => {
-  //   const todoToCancel = new CustomEvent<ITodo>('cancel-modal', {
-  //     detail: TODO_01
-  //   });
-  //   expect(todoToCancel.detail).toHaveBeenCalled();
-  // });
+  function changeBooleanTotoBeenEdited() {
+    return null;
+  }
+  it('should return null the ITodo related to modal', async () => {
+    expect(changeBooleanTotoBeenEdited()).toBe(null);
+  });
 
   it('should listen [todo-to-update] event in edit-todo component and open the modal of todo once we click on EDIT', async () => {
     const todoToShow = new CustomEvent<ITodo>('todo-to-update', {
-      detail: TODO_01
+      detail: TODO_01,
     });
     window.fetch = jest.fn();
-    page.root.dispatchEvent(todoToShow)
+    page.root.dispatchEvent(todoToShow);
     expect(fetchMock).toHaveBeenCalled();
   });
 
@@ -95,43 +88,39 @@ describe('app-home', () => {
       detail: {
         ...TODO_01,
         title: 'Eating',
-        order: 50
-      }
+        order: 50,
+      },
     });
     window.fetch = jest.fn();
-    page.root.dispatchEvent(todoEditing)
+    page.root.dispatchEvent(todoEditing);
     expect(fetchMock).toHaveBeenCalled();
   });
 
   it('should listen [add-more] event with add-todo component when we add new todo', async () => {
     const todoUpEvent = new CustomEvent<string>('add-more', {
-      detail: 'Hello'
+      detail: 'Hello',
     });
-   window.fetch = jest.fn();
-    page.root.dispatchEvent(todoUpEvent)
+    window.fetch = jest.fn();
+    page.root.dispatchEvent(todoUpEvent);
     expect(fetchMock).toHaveBeenCalled();
   });
 
   it('should listen [delete-todo] event in to-do component when we click on DELET', async () => {
     const todoToDelet = new CustomEvent<ITodo>('delete-todo', {
-      detail: TODO_01
+      detail: TODO_01,
     });
-   window.fetch = jest.fn();
-    page.root.dispatchEvent(todoToDelet)
+    window.fetch = jest.fn();
+    page.root.dispatchEvent(todoToDelet);
     expect(fetchMock).toHaveBeenCalled();
   });
 
   it('should fetch the API and show loading spinners ', async () => {
     const loading = true;
 
-   window.fetch = jest.fn();
+    window.fetch = jest.fn();
     expect(loading).toBeTruthy();
     expect(fetchMock).toHaveBeenCalled();
   });
-
 });
-
-
-
 
 // La commande: npx stencil test --spec ./src/components/app-home/test/app-home.spec.tsx --coverage
